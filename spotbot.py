e88ea9d737f033219876b7b23f4df9d5c15b5729
Coding Duck <57272996+CodingDuckmx@users.noreply.github.com>
2020-11-23 14:02:57 -0600

    working on how to manage 300k+ rows

--- spotbot.py
+++ spotbot.py
@@ -235,7 +235,7 @@ class SpotBot():
 
   
 
-    def store_songs(self,username='spotify'):
+    def store_songs(self,username='spotify', similarity_threshold = 0.84):
 
         '''
         Takes a dictionary of playlists and its songs (including their features)
@@ -522,45 +522,55 @@ class SpotBot():
 
                     cursor_outcome_2 = cursor.fetchall()
 
-                    songs_inside_subcluster = [item[0] for item in cursor_outcome_2]        
+                    if cursor_outcome_2:
 
-                    # Scale and normalize the vector song and the matrix of songs:
+                        songs_inside_subcluster = [item[0] for item in cursor_outcome_2]        
 
-                    scaled_db = scalers_list[song_dict['cluster']].transform(db)
-                    
-                    norm_db = scaled_db / np.linalg.norm(scaled_db, axis=1, keepdims=True)
-                    
-                    scaled_song_vector = scalers_list[song_dict['cluster']].transform(song_vector.reshape(1,-1))
+                        # Scale and normalize the vector song and the matrix of songs:
 
-                    norm_song_vector = scaled_song_vector / np.linalg.norm(scaled_song_vector, keepdims=True)
+                        scaled_db = scalers_list[song_dict['cluster']].transform(db)
+                        
+                        norm_db = scaled_db / np.linalg.norm(scaled_db, axis=1, keepdims=True)
+                        
+                        scaled_song_vector = scalers_list[song_dict['cluster']].transform(song_vector.reshape(1,-1))
 
-                    similarities = norm_db.dot(norm_song_vector.T)
+                        norm_song_vector = scaled_song_vector / np.linalg.norm(scaled_song_vector, keepdims=True)
 
-                    similar_songs = {}
-                    
-                    # In this moment, the db will not be updated with similarities backward. 
+                        similarities = norm_db.dot(norm_song_vector.T)
 
-                    for i, song_inside_id in enumerate(songs_inside_subcluster):
+                        similarities = np.around(similarities,decimals=4)
 
-                        if song_inside_id != song_dict['song_id']:
+                        similar_songs = {}
+                        
+                        # In this moment, the db will not be updated with similarities backward. 
 
-                            similarity = round(similarities[i][0],4)
+                        for i, song_inside_id in enumerate(songs_inside_subcluster):
 
-                            if similarity >= 0.75:
+                            if song_inside_id != song_dict['song_id']:
 
-                                if similarity not in similar_songs:
+                                if similarity >= similarity_threshold:
 
-                                    similar_songs[similarity] = []
-                                
-                                similar_songs[similarity].append(song_inside_id)
+                                    if similarity not in similar_songs:
 
-                    if similar_songs:
+                                        similar_songs[similarity] = []
+                                    
+                                    similar_songs[similarity].append(song_inside_id)
 
                         values_to_insert = (song_dict['song_id'],song_dict['song_name'], song_dict['artists'],song_dict['popularity'], song_dict['explicit'], song_dict['danceability'], song_dict['energy'], song_dict['song_key'], song_dict['loudness'], song_dict['song_mode'],
                                     song_dict['speechiness'], song_dict['acousticness'], song_dict['instrumentalness'], song_dict['liveness'], song_dict['valence'], song_dict['tempo'], song_dict['duration_ms'], song_dict['year'],song_dict['coded_artists'],str(similar_songs),
                                     song_dict['cluster'],song_dict['subcluster'])
 
                     else:
+                        
+                        count_errors += 1
+
+                        print('#######################################################################')
+                        print('#######################################################################')
+                        print('#######################################################################')
+                        print('Some songs has no similar songs dictionary. A refactoring is mandatory.')
+                        print('#######################################################################')
+                        print('#######################################################################')
+                        print('#######################################################################')
 
                         values_to_insert = (song_dict['song_id'],song_dict['song_name'], song_dict['artists'],song_dict['popularity'], song_dict['explicit'], song_dict['danceability'], song_dict['energy'], song_dict['song_key'], song_dict['loudness'], song_dict['song_mode'],
                                     song_dict['speechiness'], song_dict['acousticness'], song_dict['instrumentalness'], song_dict['liveness'], song_dict['valence'], song_dict['tempo'], song_dict['duration_ms'], song_dict['year'],song_dict['coded_artists'],None,
@@ -714,7 +724,7 @@ class SpotBot():
 
         return clustering_dict, songs_dict, model, std_scaler
 
-    def __find_similar_songs(self, clustering_dict, songs_dict, similarity_threshold):
+    def __find_similar_songs(self, clustering_dict, songs_dict, similarity_threshold = .89):
 
         features = clustering_dict['features'] 
         songs_ids = clustering_dict['songs_ids']
@@ -729,21 +739,39 @@ class SpotBot():
 
         for i, song_id in enumerate(songs_ids):
 
+            # Limit the similarities to the top 20.
+            top_similarities = []
+
             for j, similarity in enumerate(similarities_array[i]):
 
                 if similarity >= similarity_threshold:
 
                     if i != j:
 
-                        if similarity not in songs_dict[song_id]['similar_songs']:
+                        if similarity not in top_similarities:
 
-                            songs_dict[song_id]['similar_songs'][similarity] = []
+                            top_similarities.append(similarity)
 
-                        songs_dict[song_id]['similar_songs'][similarity].append(songs_ids[j])
+                            top_similarities.sort(reverse=True)
+
+                            top_similarities = top_similarities[:20]
+
+                        if similarity in top_similarities:
+
+                            if similarity not in songs_dict[song_id]['similar_songs']:
+
+                                songs_dict[song_id]['similar_songs'][similarity] = []
+
+                            songs_dict[song_id]['similar_songs'][similarity].append(songs_ids[j])
+                            
+                        else:
+
+                            if similarity in songs_dict[song_id]['similar_songs']:
+
+                                del songs_dict[song_id]['similar_songs'][similarity]
 
 
-
-    def update_similarities(self):
+    def update_similarities(self,similarity_threshold = 0.84):
 
         '''
         Pull all the db data.
@@ -795,17 +823,12 @@ class SpotBot():
 
         le = LabelEncoder()
 
-        coded_artists = le.fit_transform(artists)
+        coded_artists = [code.item() for code in le.fit_transform(artists)]
 
-        songs_features = [list(outcome[2:]) for outcome in cursor_outcome]
+        songs_features = np.array([[0] + list(outcome[3:]) for outcome in cursor_outcome], dtype=float)
 
-        for i, row in enumerate(songs_features):
+        songs_features[:,0] = coded_artists
 
-            row[0] =  coded_artists[i]
-
-            songs_dict[songs_id_list[i]]['coded_artists'] = coded_artists[i].item()
-
-        songs_features = np.array(songs_features, dtype=float)
 
         # Find the first clustering
         clustering_dict, songs_dict, model_instance, std_scaler_instance = self.__db_clustering(songs_features,len(songs_features[0])-2,songs_dict,'clustering',songs_id_list)
@@ -830,7 +853,7 @@ class SpotBot():
 
             for j in clustering_dict[i]['subclustering'].keys():
 
-                self.__find_similar_songs(clustering_dict[i]['subclustering'][j],songs_dict,0.75)
+                self.__find_similar_songs(clustering_dict[i]['subclustering'][j],songs_dict,similarity_threshold = similarity_threshold)
 
         # Pickle the final songs similarities dict.
 
@@ -914,9 +937,9 @@ if __name__ == "__main__":
     One playlist username = 8ppupllf9815lb0jzdj4dot8x
     imsammwalker
     Señora dejada lavando trastes: 12125120773
-    Angel: spotify:user:1279690720
-    Ross: spotify:user:22tbf4n6jbom3jvylao3jydbi
-    Ceci: spotify:user:12135358331
+    Angel: 1279690720
+    Ross: 22tbf4n6jbom3jvylao3jydbi
+    Ceci: 12135358331
     '''
     #####
 
@@ -930,7 +953,18 @@ if __name__ == "__main__":
                     '1298329795','pa7x3seb1p6var8o2p6tixyo7','ojleon','12122576634','12132823078','12143485078',
                     '12157507102','12168098060','1279839528','1280902170','1289035627','1295571934', 'alexgq87',
                     '2257gvsl2t64pweirfzkpsvdi', '22v4jwad7mewgwk3b7o5r4vua', 'wandererpublishing','1293411501',
-                    'victorbesq', 'rulo8817','slinky_duck']
+                    'victorbesq', 'rulo8817','slinky_duck', '113805952','1144585077','1163174336','12100733765',
+                    '12101718552','12123174013','12123368799','12131955846','12133697760','12137237711','12138092519',
+                    '12141613881','12144828589','12150418460','12168038732','12179236172','12180114917','121854533',
+                    '1276570602','1276617649','1276939404','1276989189','1277041201','1277086521','1277126844','1277434572',
+                    '1277451946','1277768181','1278315967','1279736449','1280136979','1282342893','1283472810','1285722668',
+                    '1287802341','1290109166','1291858007','1292798097','1296162383', '1298699622','1299017784','1299360987',
+                    '223rmt26ivoovwnegprr5huza','225erdwr6gl3x26btp4vcwt7y','22gpnbdkp6ycgc2klu7qcfdua','22hkrtxz47q7vaetdqa3j74sy',
+                    '22kclfntihs36oayactjyw4nq','22spuownsed6abxqadrw2k5wa','8841j15fsh70r7pe01urwdfui','adla1904','alecastroo',
+                    'car10z','conchawasthere','dopetrackz','ecortesad','felizalr','gabadiel','gonzalez.estefan','hiperactiva','hiramverarivas',
+                    'joelhzro','kjw30lf4wdewra8fuykah7gpx','ktydlr','mar-sosa','m%C3%A9xicov','p_moch','pepedelatorre','pop-rock-espa%C3%B1ol',
+                    'rahea','yusarrikitiki','tandmproductionco','sustaitaana','spookymg','samuelbmora','rorocorleone','rahea',
+                    '31kgbnb6ryhhkegmvsvs2vxxol3a','12146813420','12140114724','1298514046','2k4d5krsu7y3mxv0ax2yzlilr','6mwchrg8dfie78pqrrfhw5s8w']
 
     
     sb = SpotBot()
@@ -940,13 +974,13 @@ if __name__ == "__main__":
     # sb.store_songs()
     # sb.store_songs(username=username)
     
-    for user in username_list:
-        sb.store_songs(username=user)
+    # for user in username_list:
+    #     sb.store_songs(username=user)
     # breakpoint()
 
 
-    sb.update_similarities()
+    sb.update_similarities(similarity_threshold = 0.84)
 
-    sb.update_db_similarities_from_pickle()
+    # sb.update_db_similarities_from_pickle()
 
     # sb.find_similar_songs_from_pickle('4XTRWWCbB68WDeAHLfv2HP',10)
